b7c939ede229cc3057e16b34b02be233ac7881fb
Rafael Moreno <rafaelmorenosax@gmail.com>
2023-02-16 21:25:11 -0300

    Aula 1

---
+++ Aula1/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuSuperiorComponent } from './menu-superior/menu-superior.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuSuperiorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
